5dcebb7cd22a251984f5602c8f380a5d98d61bc7
Fran Bosquet <fran@redradix.com>
2019-02-18 17:42:41 +0100

    Get pokemon detail

--- controllers/items.js
+++ controllers/items.js
@@ -6,8 +6,6 @@ const {
   getServiceById
 } = require('../services')
 
-const 
-
 module.exports = new Router()
   .get('/', async (req, res) => {
     const data = await Promise.all([
@@ -22,9 +20,9 @@ module.exports = new Router()
     
     res.status(200).json(mergedData)
   })
-  .get('/:id', (req, res) => {
+  .get('/:id', async (req, res) => {
     const {id} = req.params
     const service = getServiceById(id)
-    const data = service.get(id)
-    res.status(200).send(type)
+    const data = await service.get(id)
+    res.status(200).send(data)
   });
--- services/pokemon.js
+++ services/pokemon.js
@@ -2,14 +2,25 @@ const axios = require('axios')
 const baseUrl = process.env.POKEMON_API_URL
 
 const extractId = url => url.match(/(?<=pokemon\/)\d+/)[0]
+const mapDataToListItem = ({name, url}) => ({
+  name,
+  id: extractId(url)
+})
+const mapDataToDetail = ({ name, id, sprites: { front_default: image }}) => ({
+  name,
+  id,
+  image
+})
 
 module.exports = {
   getAll: async () => {
     const { data: { results: pokemons } } = await axios.get(`${baseUrl}?limit=1000`)
-    const normalizedData = pokemons.map(({name, url}) => ({
-      name,
-      id: extractId(url)
-    }))
+    const normalizedData = pokemons.map(mapDataToListItem)
+    return normalizedData
+  },
+  get: async id => {
+    const {data} = await axios.get(`${baseUrl}${id}`)
+    const normalizedData = mapDataToDetail(data)
     return normalizedData
   }
 }
