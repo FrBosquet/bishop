8c4cb95f6ef22f7617b54d7c36d8f6069d498971
Fran Bosquet <fran@redradix.com>
2019-02-18 16:09:06 +0100

    Normalize pokemon data

--- services/pokemon.js
+++ services/pokemon.js
@@ -1,9 +1,15 @@
 const axios = require('axios')
 const baseUrl = process.env.POKEMON_API_URL
 
+const extractId = url => url.match(/(?<=pokemon\/)\d+/)[0]
+
 module.exports = {
   getAll: async () => {
     const { data: { results: pokemons } } = await axios.get(baseUrl)
-    return pokemons
+    const normalizedData = pokemons.map(({name, url}) => ({
+      name,
+      id: extractId(url)
+    }))
+    return normalizedData
   }
 }
