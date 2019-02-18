eee475493d4f21def33b25811032f4ad5dc91933
Fran Bosquet <fran@redradix.com>
2019-02-18 16:01:45 +0100

    :arrow_up: Add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const app = require('./app');
+const app = require('./app')
+require('dotenv').config()
 
 app
   .use('/items', require('./controllers/items'))
   .all('*', (req, res) => res.sendStatus(404))
-  .listen(3000);
+  .listen(3000)
