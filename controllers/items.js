b6eb2c2def3a6f1eed3b4c5da69c726e5731e983
Fran Bosquet <fran@redradix.com>
2019-02-19 09:40:19 +0100

    :art: expose services as a common one

--- controllers/items.js
+++ controllers/items.js
@@ -1,28 +1,16 @@
 const { Router } = require('express');
-const { 
-  pokemonService,
-  clashService,
-  driversService,
-  getServiceById
+const {
+  get,
+  getAll
 } = require('../services')
 
 module.exports = new Router()
   .get('/', async (req, res) => {
-    const data = await Promise.all([
-      pokemonService.getAll(),
-      clashService.getAll(),
-      driversService.getAll()
-    ])
-
-    const mergedData = data.reduce(
-      (acc, item) => [ ...acc, ...item],
-      [])
-    
-    res.status(200).json(mergedData)
+    const data = await getAll()
+    res.status(200).json(data)
   })
   .get('/:id', async (req, res) => {
     const {id} = req.params
-    const service = getServiceById(id)
-    const data = await service.get(id)
+    const data = await get(id)
     res.status(200).send(data)
   });
--- services/index.js
+++ services/index.js
@@ -2,18 +2,34 @@ const pokemonService = require('./pokemon')
 const clashService = require('./clash')
 const driversService = require('./drivers')
 
+const getServiceById = id => {
+  switch(true){
+    //Pokemons only have numbers in its id
+    case /^\d+$/.test(id): return pokemonService
+    //Drivers only have letters in its id
+    case /^[a-zA-Z]+$/.test(id): return driversService
+    //The rest are clash cards
+    default: return clashService
+  }
+}
+
 module.exports = {
   pokemonService,
   clashService,
   driversService,
-  getServiceById: id => {
-    switch(true){
-      //Pokemons only have numbers in its id
-      case /^\d+$/.test(id): return pokemonService
-      //Drivers only have letters in its id
-      case /^[a-zA-Z]+$/.test(id): return driversService
-      //The rest are clash cards
-      default: return clashService
-    }
+  get: async id => {
+    const service = getServiceById(id)
+    return await service.get(id)
+  },
+  getAll: async () => {
+    const data = await Promise.all([
+      pokemonService.getAll(),
+      clashService.getAll(),
+      driversService.getAll()
+    ])
+
+    return data.reduce(
+      (acc, item) => [ ...acc, ...item],
+      [])
   }
 }
